f0e678fc56793911e8f356639114fa2b1f22452e
Nick's Hermes <1572453+Nickfost@users.noreply.github.com>
2026-09-09 20:41:20 -0500

    fix: reject relative parent traversal

--- scripts/install-worker-controller.sh
+++ scripts/install-worker-controller.sh
@@ -646,6 +646,7 @@ manager_release_complete() {
 resolve_link_target() {
   local target=$1 link=$2
   if [[ "$target" != /* ]]; then
+    [[ "/$target/" != *"/../"* ]] || return 1
     target=$(realpath -ms -- "$(dirname "$link")/$target") || return 1
   fi
   printf '%s' "$target"
